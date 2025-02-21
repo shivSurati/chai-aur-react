c56dfd5a5a06abf1faea16a757c93934362167c0
shivam surati <shivamsurati04@gmail.com>
2025-02-22 00:57:54 +0530

    added 01basicreact and 01vitereact projects

---
+++ 01basicreact/src/App.js
@@ -0,0 +1,5 @@
+function App() {
+  return <h1>chai aur react | shivam </h1>;
+}
+
+export default App;
---
+++ 01vitereact/src/App.jsx
@@ -0,0 +1,5 @@
+function App() {
+  return <h1>chai aur react with vite | shivam</h1>;
+}
+
+export default App;
