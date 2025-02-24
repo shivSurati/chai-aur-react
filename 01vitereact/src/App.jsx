44422b1e481bd4bcc36a71545c0ea2fc508e080b
shivam surati <shivamsurati04@gmail.com>
2025-02-24 22:50:20 +0530

    Hooks concept in react

--- 01vitereact/src/App.jsx
+++ 01vitereact/src/App.jsx
@@ -1,9 +1,10 @@
 import Chai from "./chai";
 function App() {
+  const username = "chai aur code";
   return (
     <>
       <Chai />
-      <h1>Chai aur React</h1>
+      <h1>Chai aur React {username}</h1>
       <p>test para</p>
     </>
   );
--- 01vitereact/src/main.jsx
+++ 01vitereact/src/main.jsx
@@ -1,10 +1,31 @@
-import { StrictMode } from "react";
-import { createRoot } from "react-dom/client";
-
+import React from "react";
+import ReactDom from "react-dom/client";
 import App from "./App.jsx";
 
-createRoot(document.getElementById("root")).render(
-  <StrictMode>
-    <App />
-  </StrictMode>
+function MyApp() {
+  return (
+    <div>
+      <h1>Custom App | chai</h1>
+    </div>
+  );
+}
+
+const anotherUser = "chai aur react";
+
+const anotherElement = (
+  <a href="https://google.com" target="_blank">
+    Visit Google
+  </a>
 );
+
+const reactElement = React.createElement(
+  "a",
+  {
+    href: "https://google.com",
+    target: "_blank",
+  },
+  "click me to visit GOOGLE",
+  anotherUser
+);
+
+ReactDom.createRoot(document.getElementById("root")).render(reactElement);
